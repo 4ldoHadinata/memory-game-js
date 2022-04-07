dd7faa041400a004d6a486def07312c2a90d3cb6
4ldoHadinata <4ldohadinata@gmail.com>
2022-04-07 13:52:53 +0700

    init

---
+++ script.js
@@ -0,0 +1,40 @@
+const boxes = document.querySelectorAll('.box');
+
+let first;
+let second;
+let matchCounter = 0;
+
+boxes.forEach(function(box) {
+    let randomNum = Math.floor(Math.random() * 12);        
+    box.style.order = randomNum;
+
+    box.addEventListener('click', function() {
+        if (!first && !second) {
+            first = box;
+            box.classList.add('show');
+        } else if (first && !second) {
+            second = box;
+            box.classList.add('show');
+            if (first.innerHTML === second.innerHTML) {
+                first.style.pointerEvents = 'none';
+                second.style.pointerEvents = 'none';
+                first = null;
+                second = null;
+                matchCounter++;
+                if (matchCounter >= 6) setTimeout(() => alert('Game done! Refresh page to replay.'), 2000 );
+            } else {
+                first.classList.add('hide');
+                second.classList.add('hide');
+                setTimeout(() => {
+                    first.classList.remove('show');
+                    second.classList.remove('show');
+                    first.classList.remove('hide');
+                    second.classList.remove('hide');
+                    first = null;
+                    second = null;
+                }, 2000);
+            }
+        }
+
+    })
+});
